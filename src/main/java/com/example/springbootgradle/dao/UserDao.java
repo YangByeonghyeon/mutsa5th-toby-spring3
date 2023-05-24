413a73a6f5c8df542c5640aad1acff09e3db152f
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-24 15:06:28 +0900

    feat: feat: ConnectionMaker Interface 적용

--- src/main/java/com/example/springbootgradle/dao/UserDao.java
+++ src/main/java/com/example/springbootgradle/dao/UserDao.java
@@ -5,10 +5,14 @@ import com.example.springbootgradle.domain.User;
 import java.sql.*;
 
 public class UserDao {
-    SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
+    ConnectionMaker connectionMaker;
+    public UserDao() {
+        this.connectionMaker = new DConnectionMaker();
+    }
+    // SimpleConnectionMaker connectionMaker = new SimpleConnectionMaker();
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection conn = connectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         pstmt.setString(1, user.getId());
         pstmt.setString(2, user.getName());
@@ -20,7 +24,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection conn = connectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
         PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
         pstmt.setString(1, id);
         ResultSet rs = pstmt.executeQuery();
@@ -41,12 +45,12 @@ public class UserDao {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         User user = new User();
-        user.setId("5");
+        user.setId("6");
         user.setName("kyeongrok");
         user.setPassword("1234");
         userDao.add(user);
 
-        User selectedUser = userDao.get("5");
+        User selectedUser = userDao.get("6");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
